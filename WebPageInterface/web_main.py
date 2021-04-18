edb83ea5142e630e71c944f175d71f854f951559
ba2669x <arnoldas.baksys@fmc-ag.com>
2021-04-18 12:04:39 +0300

    -Rename files and add TODO parts

--- WebPageInterface/web_main.py
+++ WebPageInterface/web_main.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, json, url_for, request
 from werkzeug.exceptions import HTTPException
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-import rekuperat
-import schedule
+import local_control_file
 import requests
 
 app = Flask(__name__)
@@ -34,15 +33,7 @@ def _home_main_page():
 @auth.login_required
 @app.route("/test")
 def _testing():
-    # GET XML data
-
-    # # PARSE XML to JSON object
-    # # CHECK how to parse XML > JSON
-    # data = {}
-    # # validate if all manadatory data has been retrieved
-    # # validateRetrievedData(data)
-    # # validated data object
-    # # PASS the data to HTML
+    # TEST
     return render_template('login_page.html'), "Test page is working!"
 
 
@@ -50,11 +41,18 @@ def _testing():
 @app.route("/rekuperatorius")
 def _rekuperatorius():
 
-    schedule.every(1).hours.do(rekuperat._handle_login_page)  # TODO: fix with difference run
-    schedule.every(5).seconds.do(rekuperat._refresh_data)  # TODO: fix with difference run
-    schedule.run_pending()  # TODO: fix with difference run
+    # local_control_file._handle_login_page()
+    local_control_file._refresh_data()
 
     return render_template('rekup.html', title='REKUPERATORIUS')
+
+
+@app.route("/rekuperatorius", methods=["POST"])
+def _change_rekup_mode():
+    mode_options = request.args.get("mode")
+    print(mode_options)
+    local_control_file._change_rekup_mode(mode_options)  # Insert other number of Mode
+    return render_template('durys.html', title='APSAUGA')  # TODO: fix, since need to return somekind of information
 
 
 @auth.login_required
@@ -62,22 +60,6 @@ def _rekuperatorius():
 def _duru_apsauga():
     return render_template('durys.html', title='APSAUGA')
 
-
-@app.route("/rekuperatorius", methods=["POST"])
-def _xuiznajit():
-    rekup_mode = request.args.get("mode")
-    print(rekup_mode)
-    changeRekupMode(rekup_mode)  # Insert other number of Mode
-    return render_template('durys.html', title='APSAUGA')  # TODO: fix, since need to return somekind of information
-
-
-def changeRekupMode(mode):
-    url = 'http://192.168.0.200/ajax.xml'
-    myobj = {'3': mode}  # '3' - mode request || mode - (int) of selected item
-    print('Hitted')  # add Previous mode name before changing.
-    x = requests.post(url, data=myobj)
-    print(x.status_code)  # add LOG after mode is changed, what new mode it is
-    
 
 # ---------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
