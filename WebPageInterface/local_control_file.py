ebc8d6a6062737c09a02cbcfc4a3231c4b5e013c
ba2669x <arnoldas.baksys@fmc-ag.com>
2021-04-21 16:07:38 +0300

    -Update project

--- WebPageInterface/local_control_file.py
+++ WebPageInterface/local_control_file.py
@@ -1,10 +1,6 @@
-import os
-
 import requests
 import xmltodict
 import datetime
-import schedule
-import xml.etree.ElementTree as ET
 import json
 
 
@@ -14,8 +10,8 @@ _rekuperator_ip = 'http://192.168.0.200/'
 _refresh_url = 'i.asp'
 _send_request = 'ajax.xml'
 _current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-_xml_file = 'stored_rekup_data.xml'
-_json_file = 'stored_rekup_data.json'
+_xml_file = 'stored_rekup_data.xml'  # TODO: remove
+_json_file = 'server_response.json'
 
 
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
@@ -40,7 +36,7 @@ class GetData(object):
         """
 
         refreshed_data_ip = _rekuperator_ip + _refresh_url
-        refreshed_data = requests.get(url=refreshed_data_ip)
+        refreshed_data = requests.get(url=refreshed_data_ip).text
         # TODO: add handling if GET was not successful
 
         GetData.save_requested_data_to_json(data=refreshed_data)
@@ -66,77 +62,67 @@ class GetData(object):
         StoredDataFromFile.ventilation_program_state(text='Mode after')
 
     @staticmethod
-    def save_requested_data_to_json(data):
+    def save_requested_data_to_json(data: str):
         """Helper method which saves XML response to JSON file.
 
         :param data: webpage 'i.asp' response with XML data
         """
 
-        # Save 'ajax' response to file (XML)
-        converted_response = ET.fromstring(data.text)
-        saved_data = ET.ElementTree(converted_response)
-        saved_data.write(_xml_file)
-        # TODO: XML > JSON - Return JSON
-
-        with open(_xml_file, 'r') as xml_data:
-            convert = xmltodict.parse(xml_data.read())
-            xml_data.close()
-            json_data = json.dumps(convert)
-
-            # write the json data to output
-            with open(_json_file, "w") as json_file:
-                json_file.write(json_data)
-                json_file.close()
+        # Save 'i.asp' response (as string) as JSON
+        convert = xmltodict.parse(data)
+        json_data = json.dumps(convert)
+        with open(_json_file, "w") as json_file:
+            json_file.write(json_data)
 
 
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 class StoredDataFromFile(object):
     """Get sorted information of """
 
-    @staticmethod
-    def income_temp_data() -> str:
-        """Read XML file and return SUPPLY TEMPERATURE degrees
-
-        :return: str of <AI0> xml file data
-        """
-
-        tree = ET.parse(_xml_file)
-        root = tree.getroot()
-        ai0_result = None
-        for ai0 in root.findall('./AI0'):
-            ai0_result = ai0.text
-
-        return ai0_result
-
-    @staticmethod
-    def filter_data() -> str:
-        """Read XML file and return FILTER percentage
-
-        :return: str of <FCG> xml file data
-        """
-
-        tree = ET.parse(_xml_file)
-        root = tree.getroot()
-        fcg_result = None
-        for fcg in root.findall('./FCG'):
-            fcg_result = fcg.text
-
-        return fcg_result
-
-    @staticmethod
-    def humidity_data() -> str:
-        """Read XML file and return HUMIDITY percentage
-
-        :return: str of <AH> xml file data
-        """
-
-        tree = ET.parse(_xml_file)
-        root = tree.getroot()
-        ah_result = None
-        for ah in root.findall('./AH'):
-            ah_result = ah.text
-
-        return ah_result
+    # @staticmethod
+    # def income_temp_data() -> str:
+    #     """Read XML file and return SUPPLY TEMPERATURE degrees
+    #
+    #     :return: str of <AI0> xml file data
+    #     """
+    #
+    #     tree = ET.parse(_xml_file)  #TODO: Replace (xml is old)
+    #     root = tree.getroot()
+    #     ai0_result = None
+    #     for ai0 in root.findall('./AI0'):
+    #         ai0_result = ai0.text
+    #
+    #     return ai0_result
+    #
+    # @staticmethod
+    # def filter_data() -> str:
+    #     """Read XML file and return FILTER percentage
+    #
+    #     :return: str of <FCG> xml file data
+    #     """
+    #
+    #     tree = ET.parse(_xml_file)  #TODO: Replace (xml is old)
+    #     root = tree.getroot()
+    #     fcg_result = None
+    #     for fcg in root.findall('./FCG'):
+    #         fcg_result = fcg.text
+    #
+    #     return fcg_result
+    #
+    # @staticmethod
+    # def humidity_data() -> str:
+    #     """Read XML file and return HUMIDITY percentage
+    #
+    #     :return: str of <AH> xml file data
+    #     """
+    #
+    #     tree = ET.parse(_xml_file)  #TODO: Replace (xml is old)
+    #     root = tree.getroot()
+    #     ah_result = None
+    #     for ah in root.findall('./AH'):
+    #         ah_result = ah.text
+    #
+    #     return ah_result
 
     @staticmethod
     def ventilation_program_state(text: str) -> str:
@@ -146,13 +132,12 @@ class StoredDataFromFile(object):
         :return: str of <OMO> xml file data
         """
 
+        object_name = 'OMO'
+
         GetData.refresh_data_from_device()
 
-        tree = ET.parse(_xml_file)
-        root = tree.getroot()
-        omo_result = None
-        for omo in root.findall('./OMO'):
-            omo_result = omo.text
+        with open(_json_file, 'r') as read_file:
+            loaded = json.loads(_json_file)
 
-        print(_current_time, f'>> {text} - {omo_result}')  # add Previous mode name before changing.
-        return omo_result
+            # print(_current_time, f'>> {text} - {omo_result}')  # add Previous mode name before changing.
+            # return omo_result
