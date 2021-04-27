06d840ea6c5ede8a7d886bf14eab995cf00da321
ba2669x <arnoldas.baksys@fmc-ag.com>
2021-04-27 14:33:33 +0300

    -edit to color current mode grid

--- WebPageInterface/static/controls.js
+++ WebPageInterface/static/controls.js
@@ -1,12 +1,21 @@
-function changeMode(mode, elementId){
-         fetch(new Request(`http://127.0.0.1:5000/rekuperatorius?mode=${mode}`,
+var modeMapping =
+{
+    1: "away-image",
+    2: "normal-image",
+    3: "intensive-image",
+    4: "boost-image",
+};
+
+function changeMode(mode){
+       const thisId = modeMapping[mode];
+       fetch(new Request(`http://127.0.0.1:5000/rekuperatorius?mode=${mode}`,
          {
              method: "POST",
              headers: {"Access-Control-Allow-Origin": "true"}
          }
        )).then(response => {
            if (!response.ok) throw Error(response.statusText)
-           coloring(elementId);
+           coloring(thisId);
            return response;
         }).catch(error => console.log(error));
 }
@@ -24,11 +33,15 @@ function loadedDataDa() {
     const fetchPromise = fetch("http://127.0.0.1:5000/rekuperatorius/data");
     fetchPromise.then(response => {
     return response.json();
-    }).then(test => {document.getElementById("current-humidity").innerHTML = test.humidity;
-                     document.getElementById("current-supply").innerHTML = test.supply;
-                     document.getElementById("current-filter").innerHTML = test.filter;
-                     document.getElementById("current-eco").innerHTML = test.eco;
-                     document.getElementById("current-heater").innerHTML = test.heater;
+    }).then(test => {
+        document.getElementById("current-humidity").innerHTML = test.humidity;
+        document.getElementById("current-supply").innerHTML = test.supply;
+        document.getElementById("current-filter").innerHTML = test.filter;
+        document.getElementById("current-eco").innerHTML = test.eco;
+        document.getElementById("current-heater").innerHTML = test.heater;
+        let mode = test.curr_mode;
+        let elementId = modeMapping[mode];
+        coloring(elementId);
     });
     }
 
